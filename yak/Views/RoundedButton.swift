a25e79446238ea4a9dbb8980a89d7e22eebfc966
Jessica Joseph <jessjeanjo@gmail.com>
2018-04-04 09:37:30 -0400

    add rounded button view

---
+++ yak/Views/RoundedButton.swift
@@ -0,0 +1,26 @@
+//  Created by Jessica Joseph on 4/4/18.
+//  Copyright © 2018 TFH Inc. All rights reserved.
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
