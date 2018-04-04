e89c731182aa0bbbafb072e295d4c891b4a9153a
Jessica Joseph <jessjeanjo@gmail.com>
2018-04-04 11:41:41 -0400

    add avatar picker ui vc

---
+++ yak/Controllers/AvatarPickerController.swift
@@ -0,0 +1,13 @@
+//  Created by Jessica Joseph on 4/4/18.
+//  Copyright © 2018 TFH Inc. All rights reserved.
+
+import UIKit
+
+class AvatarPickerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
