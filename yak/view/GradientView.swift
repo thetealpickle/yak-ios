275053ebd2fa43d45540c4e2e8954d1778d9943c
Jessica Joseph <jessjeanjo@gmail.com>
2018-04-03 20:36:59 -0400

    set up the ui of the channel sliding menu and gradient

---
+++ yak/view/GradientView.swift
@@ -0,0 +1,30 @@
+//  Created by Jessica Joseph on 4/3/18.
+//  Copyright © 2018 TFH Inc. All rights reserved.
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = UIColor.cyan {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
